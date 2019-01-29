75d9720c03732fbc83510249a6d80383d128f106
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-29 23:23:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,20 +20,27 @@ return (age <= 19 && age >= 13) ? "You are a teenager" : "You are not a teenager
 function switchAge(age) {
   switch(age) {
     case "13":
-      return "You are a teenager!"
-      case "14":
-        return "You are a teenager!"
+    console.log("You are a teenager");
+        break;
+        case "14":
+        console.log("You are a teenager");
+            break;
         case "15":
-          return "You are a teenager!"
+        console.log("You are a teenager");
+            break;
           case "16":
-            return "You are a teenager!"
+          console.log("You are a teenager");
+              break;
             case "17":
-              return "You are a teenager!"
+            console.log("You are a teenager");
+                break;
               case "18":
-                return "You are a teenager!"
+              console.log("You are a teenager");
+                  break;
                 case "19":
-                  return "You are a teenager!"
+                console.log("You are a teenager");
+                    break;
     default:
-    return "You have an age"
+    console.log("You have an age")
   }
 }
