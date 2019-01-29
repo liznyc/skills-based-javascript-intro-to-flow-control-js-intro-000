016ad2f132b0480903a132255a349185eed24ecd
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-29 23:20:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,9 +14,26 @@ else if (age <= 12) {return "You are a kid"}
 }
 
 function ternaryTeenager(age) {
-
+return (age <= 19 && age >= 13) ? "You are a teenager!" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case "13":
+      return "You are a teenager!"
+      case "14":
+        return "You are a teenager!"
+        case "15":
+          return "You are a teenager!"
+          case "16":
+            return "You are a teenager!"
+            case "17":
+              return "You are a teenager!"
+              case "18":
+                return "You are a teenager!"
+                case "19":
+                  return "You are a teenager!"  
+    default:
+    return "You have an age"  
+  }
 }
